424f59fbb5ea720ff34ebd27874245c55d1385b1
Jeff Poyzner <holidaydoctor@hotmail.com>
2017-05-10 23:53:09 -0700

    uploading

---
+++ scroll.js
@@ -0,0 +1,11 @@
+$(function() {
+//	var image = new Image();
+//    image.src = "scroll.png";
+//    image.width = width * scale;
+//    image.height = height * scale;
+//
+//    document.getElementById('info').innerHTML = width + 'x' + height + ' => ' + image.width + 'x' + image.height;
+//
+//    document.getElementById('container').style.backgroundImage = 'url(' + image.src + ')';
+//    document.getElementById('container').style.backgroundSize = image.width + 'px ' + image.height + 'px';
+});
